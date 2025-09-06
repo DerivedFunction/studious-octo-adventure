f49b7e0bbae6e814f31d9e7d74cba58ae2a346b6
Denny Li <ytdli08@gmail.com>
2025-09-06 14:44:15 -0400

    expanded selection

--- chatbot-app/src/script/export.js
+++ chatbot-app/src/script/export.js
@@ -1461,7 +1461,7 @@ window.ChatGPTExport = (() => {
   function addPrintButton() {
     // Find the target elements that don't have the print button attached:
     const btn = document.querySelectorAll(
-      '[aria-label="Good response"]:not(.attached)'
+      'article [aria-label="Copy"]:not(.attached)'
     );
     if (btn.length > 0) {
       btn.forEach((btn) => {
