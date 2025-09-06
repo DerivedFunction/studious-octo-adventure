e9fef41f8075b1d004b96b595ae44a4e0759cdee
Denny Li <ytdli08@gmail.com>
2025-09-06 15:07:02 -0400

    fix button location

--- chatbot-app/src/script/export.js
+++ chatbot-app/src/script/export.js
@@ -1465,7 +1465,7 @@ window.ChatGPTExport = (() => {
   function addPrintButton() {
     // Find the target elements that don't have the print button attached:
     const btn = document.querySelectorAll(
-      'article [aria-label="Copy"]:not(.attached)'
+      'article .justify-start [aria-label="Copy"]:not(.attached)'
     );
     if (btn.length > 0) {
       btn.forEach((btn) => {
